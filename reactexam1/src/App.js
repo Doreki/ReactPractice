533e3b7f9cfda302ef034541c62d78ae596e2e9a
Doreki <idh0325@naver.com>
2023-03-11 13:23:32 +0900

    JSX

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -1,12 +1,32 @@
-import "./App.css";
+import React from "react";
+// import "./App.css";
+
+import MyHeader from "./MyHeader";
 
 function App() {
   let name = "임도현";
+
+  const style = {
+    App: {
+      backgroundColor: "black",
+    },
+    h2: {
+      color: "red",
+    },
+    bold_text: {
+      color: "green",
+    },
+  };
+
+  const number = 5;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>안녕 {name}</h2>
-      </header>
+    <div style={style.App}>
+      <MyHeader />
+      <h2 style={style.h2}>안녕 {name}</h2>
+      <b id="bold_text" style={style.bold_text}>
+        {number} 는 : {number % 2 === 0 ? "짝수" : "홀수"}
+      </b>
     </div>
   );
 }
