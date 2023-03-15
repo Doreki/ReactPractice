82758a5406ba6e927c10a8562d282ca97ff27457
Doreki <idh0325@naver.com>
2023-03-15 21:57:07 +0900

    프로젝트 초기 셋팅

---
+++ emotion-diary/src/pages/Edit.js
@@ -0,0 +1,39 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSerchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log(id);
+
+  const mode = searchParams.get("mode");
+  console.log(mode);
+
+  return (
+    <div>
+      <h1>Edit</h1>
+      <p>이곳은 일기 수정 페이지 입니다.</p>
+      <button onClick={() => setSerchParams({ who: "winterlood" })}>
+        QS 바꾸기
+      </button>
+
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        home으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
+    </div>
+  );
+};
+
+export default Edit;
